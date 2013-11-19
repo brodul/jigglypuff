ca08bd8d2fd537a5f4075955fd1976681167b5d5
Andraz Brodnik <brodul@brodul.org>
2013-11-19 22:30:11 +0100

    Revert "Move db location and media folder."
    This reverts commit 9acd12282b1753de10f2ea45c212985295738c2b.

--- yodl/__init__.py
+++ yodl/__init__.py
@@ -26,7 +26,7 @@ def main(global_config, **settings):
         session_factory=my_session_factory
     )
     config.add_static_view('static', 'static', cache_max_age=3600)
-    config.add_static_view('media', '../../media', cache_max_age=3600)
+    config.add_static_view('media', 'media', cache_max_age=3600)
     config.add_route('home', '/')
     config.include('pyramid_jinja2')
     config.scan()
